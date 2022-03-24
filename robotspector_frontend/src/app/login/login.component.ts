cf7cdbbb053ecc4877529103d451abd64d091760
Leigham <leigham.springer-sutton@cgi.com>
2022-03-24 17:07:59 -0400

    added regex

--- robotspector_frontend/src/app/login/login.component.ts
+++ robotspector_frontend/src/app/login/login.component.ts
@@ -11,44 +11,31 @@ export class LoginComponent {
     username = new FormControl();
     password = new FormControl();
     form: any;
-    submitMessage:any;
+    submitMessage: any;
     
     constructor(private formBuilder:FormBuilder,private service:AuthenticationService, private router:RouterService) {}
     //one space between methods
     ngOnInit() {
+      console.log("working");
       this.form = this.formBuilder.group({
      
-        "username": this.username,
-        "password": this.password,
+       
+        "username": ['', [Validators.required,Validators.minLength(8)]],
+        "password": ['', [Validators.required, Validators.minLength(8), Validators.pattern(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/)]]
         
      })
     }
 
     loginSubmit() {
-      
+      console.log(`log in clicked ${this.form.valid}}`);
+      console.log(this.form.value     );
       if (this.form.valid == false ){
         
-        this.submitMessage = "Title and Description should not be empty!!! Please verify details"
+        this.submitMessage = "Username or password invalid"
         return;
+      }else{
+        this.submitMessage = "";
       }
      
-      this.service.authenticateUser(this.form.value).subscribe( (data:any) =>{
-        
-        if (data!=null){
-        
-         this.service.setBearerToken(data['token']);
-        }
-      },
-      (error:Error) =>{
-        if (error!= null){
-          if (error.message.includes("403")){
-            this.submitMessage = "Unauthorized";
-            return;
-          }
-          this.submitMessage = error.message;
-        }
-      }
-      
-      )
     }
 }

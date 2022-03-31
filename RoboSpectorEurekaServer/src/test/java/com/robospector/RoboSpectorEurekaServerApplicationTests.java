008754860d5af5f18f6a78809ec377097fe3203d
vinu.muthukumar <vinu.muthukumar@cgi.com>
2022-03-31 17:49:15 -0400

    Eureka server

---
+++ RoboSpectorEurekaServer/src/test/java/com/robospector/RoboSpectorEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robospector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboSpectorEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

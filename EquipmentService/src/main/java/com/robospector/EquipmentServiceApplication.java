a6ca8c81f5cf8f55bacc4db504b2085eeb0ac6c9
herve.bagalwa <herve.bagalwa@cgi.com>
2022-04-06 12:06:46 -0400

    Swagger documentation added

--- EquipmentService/src/main/java/com/robospector/EquipmentServiceApplication.java
+++ EquipmentService/src/main/java/com/robospector/EquipmentServiceApplication.java
@@ -5,9 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@OpenAPIDefinition (info = @Info(
+		  title = "EquipmentService Crud API",
+		  version = "1.0",
+		  description = "Documentation for EquipmentService Crud API Services",
+		  contact = @Contact(
+				  name = "Leigham Springer-sutton, Vinu Muthukumar, Herve Bagalwa",
+				  url = "www.cgi.com"
+				  ),
+		  license = @License(
+				  name = "cgi",
+				  url = "www.cgi.com"
+				  ),
+		  termsOfService = "Copyright @CGI 2022"
+		))
 public class EquipmentServiceApplication {
 
 	public static void main(String[] args) {
--- InspectionMicroService/src/main/java/com/robospector/InspectionMicroServiceApplication.java
+++ InspectionMicroService/src/main/java/com/robospector/InspectionMicroServiceApplication.java
@@ -5,9 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@OpenAPIDefinition (info = @Info(
+		  title = "InspectionService Crud API",
+		  version = "1.0",
+		  description = "Documentation for InspectionService Crud API Services",
+		  contact = @Contact(
+				  name = "Leigham Springer-sutton, Vinu Muthukumar, Herve Bagalwa",
+				  url = "www.cgi.com"
+				  ),
+		  license = @License(
+				  name = "cgi",
+				  url = "www.cgi.com"
+				  ),
+		  termsOfService = "Copyright @CGI 2022"
+		))
 public class InspectionMicroServiceApplication {
 
 	public static void main(String[] args) {

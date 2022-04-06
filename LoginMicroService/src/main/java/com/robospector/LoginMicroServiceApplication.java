f3a690fdcb5da1236a224508fe96bd2e68281c3f
herve.bagalwa <herve.bagalwa@cgi.com>
2022-04-06 09:37:11 -0400

    Swagger docs added for all microsrvices

--- LoginMicroService/src/main/java/com/robospector/LoginMicroServiceApplication.java
+++ LoginMicroService/src/main/java/com/robospector/LoginMicroServiceApplication.java
@@ -3,7 +3,26 @@ package com.robospector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition (info = @Info(
+		  title = "LoginService Crud API",
+		  version = "1.0",
+		  description = "Documentation for LoginService Crud API Services",
+		  contact = @Contact(
+				  name = "Leigham Springer-sutton, Vinu Muthukumar, Herve Bagalwa",
+				  url = "www.cgi.com"
+				  ),
+		  license = @License(
+				  name = "cgi",
+				  url = "www.cgi.com"
+				  ),
+		  termsOfService = "Copyright @CGI 2022"
+		))
 public class LoginMicroServiceApplication {
 
 	public static void main(String[] args) {
